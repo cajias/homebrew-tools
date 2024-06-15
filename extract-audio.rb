2b3401d4484c5131dcb8f6f30833638673a03f4a
Raul <rau.cajiasl@gmail.com>
2024-06-15 21:14:28 +0200

    [fix] update tool location

--- extract-audio.rb
+++ extract-audio.rb
@@ -1,7 +1,7 @@
 class ExtractAudio < Formula
     desc "A script to extract audio from media files"
-    homepage "https://github.com/cajias/homebrew-tools"
-    url "https://github.com/cajias/homebrew-tools/archive/v1.0.0.tar.gz"
+    homepage "https://github.com/cajias/v2a"
+    url "https://github.com/cajias/v2a/archive/v1.0.0.tar.gz"
     sha256 "ee07fcc5cd2b203e9a7695aad2d4fbe8a10fac783d612011e22471b1d66ba5c3"
     version "1.0.0"
   
